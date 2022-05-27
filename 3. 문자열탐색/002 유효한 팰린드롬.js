74a7bef6760dec77be2a4f39963b52389c7ec18f
KIMMINSIK <we_in@naver.com>
2022-05-27 18:07:12 +0900

    clear

--- 3. 문자열탐색/002 유효한 팰린드롬.js
+++ 3. 문자열탐색/002 유효한 팰린드롬.js
@@ -1,1 +1,10 @@
-d;
+//정규 표현식을 이용한 문제풀이
+function solution(s) {
+  let answer = "YES";
+  s = s.toLowerCase().replace(/[^a-z]/g, "");
+  if (s.split("").reverse().join("") !== s) return "NO";
+  return answer;
+}
+
+let str = "found7, time: study; Yduts; emit, 7Dnuof";
+console.log(solution(str));
