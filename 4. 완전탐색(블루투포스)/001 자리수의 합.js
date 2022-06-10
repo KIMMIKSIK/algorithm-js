d39c9affb676eda66c6abd40f053b0bfb63cd5ed
KIMMINSIK <we_in@naver.com>
2022-06-10 18:43:47 +0900

    clear

--- 4. 완전탐색(블루투포스)/001 자리수의 합.js
+++ 4. 완전탐색(블루투포스)/001 자리수의 합.js
@@ -1,1 +1,67 @@
-d;
+//나의 풀이
+function solution(n, arr) {
+  let answer = [];
+  let add = arr.map((item) => {
+    let total = 0;
+    let word = "" + item;
+    for (let i = 0; i < word.length; i++) {
+      total += parseInt(word[i]);
+    }
+    return total;
+  });
+  let max = Math.max(...add);
+  for (let i = 0; i < add.length; i++) {
+    if (max === add[i]) {
+      answer.push(arr[i]);
+    }
+  }
+  return Math.max(...answer);
+}
+let arr = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr));
+
+// reduce를 활용한 다른 풀이
+function solution(n, arr) {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let x of arr) {
+    let sum = x
+      .toString()
+      .split("")
+      .reduce((a, b) => a + Number(b), 0);
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+}
+
+let arr1 = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr1));
+
+// 문제 해답
+function solution(n, arr) {
+  let answer,
+    max = Number.MIN_SAFE_INTEGER;
+  for (let x of arr) {
+    let sum = 0,
+      tmp = x;
+    while (tmp) {
+      sum += tmp % 10;
+      tmp = Math.floor(tmp / 10);
+    }
+    if (sum > max) {
+      max = sum;
+      answer = x;
+    } else if (sum === max) {
+      if (x > answer) answer = x;
+    }
+  }
+  return answer;
+}
+
+let arr2 = [128, 460, 603, 40, 521, 137, 123];
+console.log(solution(7, arr2));
