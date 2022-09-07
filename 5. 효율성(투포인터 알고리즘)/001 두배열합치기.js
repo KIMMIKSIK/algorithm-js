7eef2c9d47e4b9a6529eed3f4612405e235a12cd
KIMMINSIK <we_in@naver.com>
2022-09-07 19:06:31 +0900

    5-2 edit

--- 5. 효율성(투포인터 알고리즘)/001 두배열합치기.js
+++ 5. 효율성(투포인터 알고리즘)/001 두배열합치기.js
@@ -4,22 +4,10 @@ function solution(arr1, arr2) {
   let m = arr2.length;
   let p1 = (p2 = 0);
   while (p1 < n && p2 < m) {
-    if (arr1[p1] <= arr2[p2]) {
-      stack.push(arr1[p1]);
-      p1++;
-    } else {
-      stack.push(arr2[p2]);
-      p2++;
-    }
+    if (arr1[p1] <= arr2[p2]) stack.push(arr1[p1++]);
+    else stack.push(arr2[p2++]);
   }
-  if (p1 === n) {
-    for (let i = p2; i < m; i++) {
-      stack.push(arr2[i]);
-    }
-  } else {
-    for (let i = p1; i < n; i++) {
-      stack.push(arr1[i]);
-    }
-  }
+  while (p1 < n) stack.push(arr2[p1++]);
+  while (p2 < m) stack.push(arr1[p2++]);
   return stack;
 }
